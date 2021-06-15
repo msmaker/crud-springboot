f239f14b0d8bff4ac7e20d64da1cd089181ca153
Marco Silva <marcosilva.tec@gamil.com>
2021-06-15 15:43:10 -0300

    Delete

--- src/main/java/com/lu2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/lu2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,8 @@ package com.lu2code.springboot.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.DomainEvents;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,6 +70,23 @@ public class EmployeeRestController {
 		employeeService.save(theEmployee);
 
 		return theEmployee;
+	}
+
+	// add mapping for DELETE /employees/{employeeId} - delete employees
+
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		Employee tempEmployee = employeeService.findById(employeeId);
+
+		// throw exception id null
+
+		if (tempEmployee == null) {
+			throw new RuntimeException("Exception id not found - " + employeeId);
+		}
+
+		employeeService.deleteById(employeeId);
+
+		return "Deletete employee id - " + employeeId;
 	}
 
 }
